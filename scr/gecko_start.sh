cfe297801613ecc0654f5b9975cb2d839d0e8b2d
Ubuntu <fcabrera.all@gmail.com>
2022-04-04 03:15:35 +0000

    fix gecko_start.sh

--- scr/gecko_start.sh
+++ scr/gecko_start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # vars
+count=0
 coin="btc"
 value="44000"
 
-while true; do
+while [ $count -le 3 ]; do
+	((count++))
 	python3 gecko.py -c ${coin} -v ${value} >> /opt/coin-gecko/logs/gecko.${coin}.log
 	sleep 300
 done
